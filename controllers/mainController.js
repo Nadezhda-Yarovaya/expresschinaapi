c29dc795ffac10eafa0919b9890005ae522eb559
unknown <seriozz@yandex.ru>
2024-08-07 16:23:48 +0300

    expanding routes, route daily has error yet

---
+++ controllers/mainController.js
@@ -0,0 +1,16 @@
+const Card = require("../models/Card");
+
+module.exports.getCardsByNumber =  (req, res, next) => {
+    const { numb } = req.params;
+    Card.find({})
+    .then((alltheCards) => {
+        alltheCards.sort( () => .5 - Math.random() ); // saying its a bad solution for shuffle
+        const cutCards = alltheCards.slice(0, numb);
+    console.log('alltheCards works', alltheCards);
+    res.status(200).send(alltheCards);
+    })
+    .catch((err) => console.log(err));
+    res.send(movies);
+
+}
+    
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const routes = express.Router();
 
 const { authRouter } = require('./authRouter');
 const { setRouter } = require('./settingsRouter');
+const { mainRouter } = require('./mainRouter');
 const { notFoundRouter } = require('./notFound');
 
 routes.use(authRouter);
 routes.use(setRouter);
+routes.use(mainRouter);
 routes.use(notFoundRouter);
 
 module.exports = routes;
